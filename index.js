cb57b8892219d0f46aeea48d2c032da2ee33b3c6
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-23 19:37:09 -0700

    commit tree c6cfbd49d45bcbb54391f83b0ce5d4188699b72d

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = function(specialChar = "*") {
+  return function(defaultArg = "special") {
+    return `You are ${specialChar}${defaultArg}${specialChar}!`
+  }
+}
+
+const Calculator = {
+  add: function(num1, num2) {
+    return num1 + num2;
+  },
+
+  subtract: function(num1, num2) {
+    return num1 - num2;
+  },
+
+  multiply: function(num1, num2) {
+    return num1 * num2;
+  },
+
+  divide: function(num1, num2) {
+    return num1 / num2;
+  }
+}
+
+const actionApplyer = function(initValue, functionsArr) {
+  let result = initValue;
+
+  for (let i = 0; i < functionsArr.length; i++ ){
+    result = functionsArr[i](result);
+  }
+
+  return result;
+}
